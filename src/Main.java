695ef9377d6610af7a2c667389accdaabfad2e09
Aleksey96 <azolotuhin96@gmail.com>
2021-07-14 07:39:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import static java.lang.System.out;
 public class Main {
     public static void main(String[] args) {
         out.println("Hellow world!");
-        out.println("do git fetch");
+        //out.println("do git fetch");
     }
 }
